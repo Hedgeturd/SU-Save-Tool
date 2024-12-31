fd27f6252d3cded3792a700351b5dca752e46ac1
Mahad <110350143+Hedgeturd@users.noreply.github.com>
2024-12-31 23:35:31 +0000

    Add files via upload

--- Platform.cs
+++ Platform.cs
@@ -1,27 +1,47 @@
 namespace SonicUnleashedFCOConv {
     public static class Platform {
-        public static bool structureError = false, PS3 = false;
+        public static bool PS3 = false;
         public static long address;
 
         public static void ReadSave(string path) {
             FileStream fileStream = new FileStream(path, FileMode.Open, FileAccess.Read);
             BinaryReader binaryReader = new BinaryReader(fileStream);
 
-            long header = binaryReader.ReadInt64();
-            if (header != 144115188075888703) {
-                structureError = true;
-                address = binaryReader.BaseStream.Position;
-                return;
-            }
+            try
+            {
+                //long header = binaryReader.ReadInt64();
+                if (binaryReader.ReadInt64() != 144115188075888703) {
+                    //structureError = true;
+                    //address = binaryReader.BaseStream.Position;
+                    //return;
+                
+                    Console.WriteLine("\nERROR: Exception occurred during parsing at: 0x" + unchecked((int)binaryReader.BaseStream.Position).ToString("X")  + ".");
+                    Console.WriteLine("Please make sure your Save file is Decrypted!");
+                    Console.WriteLine("\nPress Enter to Exit.");
+                    Console.Read();
+                    
+                    return;
+                }
 
-            if (binaryReader.BaseStream.Length == 0x9150) {
-                Console.WriteLine("PS3 Save Detected!");
-                PS3 = true;
+                if (binaryReader.BaseStream.Length == 0x9150) {
+                    Console.WriteLine("PS3 Save Detected!");
+                    PS3 = true;
+                }
+                if (binaryReader.BaseStream.Length == 0xA950) {
+                    Console.WriteLine("X360 Save Detected!");
+                }
             }
-            if (binaryReader.BaseStream.Length == 0xA950) {
-                Console.WriteLine("X360 Save Detected!");
+            catch (EndOfStreamException e) {
+                Console.WriteLine(e);
+                
+                Console.WriteLine("\nERROR: Exception occurred during parsing at: 0x" + unchecked((int)binaryReader.BaseStream.Position).ToString("X")  + ".");
+                Console.WriteLine("There is a structural abnormality within the Save file!");
+                Console.WriteLine("\nPress Enter to Exit.");
+                Console.Read();
+                
+                throw;
             }
-			
+            
             fileStream.Close();
             binaryReader.Close();
             Console.WriteLine("Save Read!");
