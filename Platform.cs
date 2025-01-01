2e27bd0f220734458e4f630694809b6f458152b7
Hedgeturd <supercooper_32.1@hotmail.com>
2025-01-01 00:28:43 +0000

    Simplified Read and Write

--- Platform.cs
+++ Platform.cs
@@ -1,45 +1,34 @@
 namespace SonicUnleashedFCOConv {
     public static class Platform {
         public static bool PS3 = false;
-        public static long address;
 
         public static void ReadSave(string path) {
             FileStream fileStream = new FileStream(path, FileMode.Open, FileAccess.Read);
             BinaryReader binaryReader = new BinaryReader(fileStream);
-
-            try
-            {
-                //long header = binaryReader.ReadInt64();
-                if (binaryReader.ReadInt64() != 144115188075888703) {
-                    //structureError = true;
-                    //address = binaryReader.BaseStream.Position;
-                    //return;
-                
-                    Console.WriteLine("\nERROR: Exception occurred during parsing at: 0x" + unchecked((int)binaryReader.BaseStream.Position).ToString("X")  + ".");
-                    Console.WriteLine("Please make sure your Save file is Decrypted!");
-                    Console.WriteLine("\nPress Enter to Exit.");
-                    Console.Read();
-                    
-                    return;
-                }
-
-                if (binaryReader.BaseStream.Length == 0x9150) {
-                    Console.WriteLine("PS3 Save Detected!");
-                    PS3 = true;
-                }
-                if (binaryReader.BaseStream.Length == 0xA950) {
-                    Console.WriteLine("X360 Save Detected!");
-                }
-            }
-            catch (EndOfStreamException e) {
-                Console.WriteLine(e);
-                
+            
+            if (binaryReader.ReadInt64() != 144115188075888703) {
                 Console.WriteLine("\nERROR: Exception occurred during parsing at: 0x" + unchecked((int)binaryReader.BaseStream.Position).ToString("X")  + ".");
-                Console.WriteLine("There is a structural abnormality within the Save file!");
+                Console.WriteLine("Please make sure your Save file is Decrypted!");
                 Console.WriteLine("\nPress Enter to Exit.");
                 Console.Read();
-                
-                throw;
+                Environment.Exit(1);
+            }
+
+            switch (binaryReader.BaseStream.Length) {
+                case 0x9150:
+                    Console.WriteLine("PS3 Save Detected!");
+                    PS3 = true;
+                    break;
+                case 0xA950:
+                    Console.WriteLine("X360 Save Detected!");
+                    break;
+                default:
+                    Console.WriteLine("\nERROR: Save file size: 0x" + unchecked((int)binaryReader.BaseStream.Length).ToString("X") + " does not match any Save file!");
+                    Console.WriteLine("There is a structural abnormality within the Save file!");
+                    Console.WriteLine("\nPress Enter to Exit.");
+                    Console.Read();
+                    Environment.Exit(1);
+                    break;
             }
             
             fileStream.Close();
@@ -53,20 +42,14 @@ namespace SonicUnleashedFCOConv {
             FileStream fileStream = new FileStream(path, FileMode.Open, FileAccess.Read);
             BinaryReader binaryReader = new BinaryReader(fileStream);
             BinaryWriter binaryWriter = new BinaryWriter(File.Open(filePath + "_CONV", FileMode.OpenOrCreate));
-
-
+            
+            for (int i = 0; i < 9300; i++) {
+                binaryWriter.Write(binaryReader.ReadInt32());
+            }
             if (PS3 == true) {
-                for (int i = 0; i < 9300; i++) {
-                    binaryWriter.Write(binaryReader.ReadInt32());
-                }
                 binaryWriter.BaseStream.Position = 0x9150;
                 for (int i = 0; i < 1536; i++) {
                     binaryWriter.Write(0x00000000);
-                }
-            }
-            else {
-                for (int i = 0; i < 9300; i++) {
-                    binaryWriter.Write(binaryReader.ReadInt32());
                 }
             }
 
